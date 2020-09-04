7cce01ba4f25e264bd7f41c6d6b413e70d1cf506
student <student@C251U05>
2020-09-04 10:51:40 +0300

    1

--- catty/Program.cs
+++ catty/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
-namespace catty
+namespace console1
 {
+  struct Person
+  {
+    public string firstName;
+    public string lastName;
+    public int age;
+
+    public Person(string _firstName, string _lastName, int _age)
+    {
+      firstName = _firstName;
+      lastName = _lastName; age = _age;
+    }
+    public override string ToString()
+    {
+      return firstName + " " + lastName + ", age " + age;
+    }
+  }
   class Program
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      Console.WriteLine("Hell9o World!");
+      Person p = new Person("Tony", "Allen", 32); Console.WriteLine(p);
     }
   }
 }
+© 2020 GitHub, Inc.
